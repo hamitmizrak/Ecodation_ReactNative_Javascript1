dde06d54123983daf88a3d93d80944eb4bfe4002
Hamit Mızrak <hamitmizrak@gmail.com>
2023-09-04 17:16:48 +0300

    js

---
+++ template.js
@@ -0,0 +1,14 @@
+// single comment
+/* multiple comment */
+
+// window.alert("Merhabalar");
+// window.document.writeln("ekranda göster");
+// window.console.log("console çıktısı");
+
+console.log("log console çıktısı");
+console.warn("warn console çıktısı");
+console.error("error console çıktısı");
+
+// variable
+var globalState="Merhabalar Güzel insanlar";
+console.log(globalState);
