b8ff1b1dc7e6a8adae345c5a5d2f5936cf393ff8
Hamit Mızrak <hamitmizrak@gmail.com>
2023-09-04 17:19:49 +0300

    js

--- template.js
+++ template.js
@@ -12,3 +12,18 @@ console.error("error console çıktısı");
 // variable
 var globalState="Merhabalar Güzel insanlar";
 console.log(globalState);
+var globalState="asdasdasd";
+console.log(globalState);
+
+let localState="Nasılsınız";
+console.log(localState);
+localState="Nasılsınız44";
+console.log(localState);
+
+const locaState2=141551;
+console.log(locaState2);
+
+// locaState2=8585;
+// console.log(locaState2);
+
+//////////////////////////////////////////
